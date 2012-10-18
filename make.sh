da723015fe489092cbc2228b5a333918c4ee86de
Brendan Heywood <brendan.heywood@gmail.com>
2012-10-18 17:39:01 +1100

    added make file

---
+++ make.sh
@@ -0,0 +1,15 @@
+
+# this just cherry picks stuff to deploy into the static files
+
+# run make FIRST
+
+DIR="../../static/bootstrap/2.1.1-c1"
+
+mkdir -p $DIR
+
+cp docs/assets/css/bootstrap.css $DIR
+cp docs/assets/css/bootstrap-responsive.css $DIR
+
+
+
+
