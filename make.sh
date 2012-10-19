1c5f3c5c43946763cc1322f7ce1c3145222ae2bb
Brendan Heywood <brendan.heywood@gmail.com>
2012-10-19 11:41:49 +1100

    images

--- make.sh
+++ make.sh
@@ -6,9 +6,12 @@
 DIR="../../static/bootstrap/2.1.1-c1"
 
 mkdir -p $DIR
+mkdir -p $DIR/css
+mkdir -p $DIR/img
 
-cp docs/assets/css/bootstrap.css $DIR
-cp docs/assets/css/bootstrap-responsive.css $DIR
+cp docs/assets/css/bootstrap.css $DIR/css
+cp docs/assets/css/bootstrap-responsive.css $DIR/css
+cp img/* $DIR/img
 
 
 
